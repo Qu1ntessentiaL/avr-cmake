4281c2678708fc1621dc64957bc6c524144e00cb
valiullin_fi <vfi@eleps.ru>
2025-09-10 15:09:57 +0300

    Добавил минимальный набор файлов

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+    // Настраиваем PE7 как выход
+    DDRE |= (1 << PE0);
+
+    while (1) {
+        // Включаем LED
+        PORTE |= (1 << PE0);
+        _delay_ms(200);
+
+        // Выключаем LED
+        PORTE &= ~(1 << PE0);
+        _delay_ms(200);
+    }
+}
