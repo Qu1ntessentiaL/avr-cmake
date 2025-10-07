08cf59267747ae122ede0c58b9e726c20278659a
valiullin_fi <vfi@eleps.ru>
2025-10-07 12:26:22 +0300

    Изменил мигание с помощью прерывания

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
 #include <util/delay.h>
+
+// Обработчик прерывания таймера
+ISR(TIMER1_COMPA_vect) {
+    // Инвертируем PE0
+    PORTE ^= (1 << PE0);
+}
 
 int main() {
     // Настраиваем PE0 как выход
     DDRE |= (1 << PE0);
 
-    while (1) {
-        // Включаем LED
-        PORTE |= (1 << PE0);
-        _delay_ms(200);
+    // Настройка Timer1
+    TCCR1B |= (1 << WGM12); // CTC mode (сравнение с OCR1A)
+    OCR1A = F_CPU / 4096;   // Значение, до которого считает счетчик
+    TCCR1B |= (1 << CS12) | (1 << CS10); // Делитель = 1024
+    TIMSK |= (1 << OCIE1A);   // Разрешаем прерывание по совпадению OCR1A
 
-        // Выключаем LED
-        PORTE &= ~(1 << PE0);
-        _delay_ms(200);
+    sei(); // Включаем глобальные прерывания
+
+    while (1) {
+        // Основной цикл пустой — всё делает прерывание
     }
 }
